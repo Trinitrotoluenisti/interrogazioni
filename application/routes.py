c759a781e213822cf8693968234e70a5428c655c
gianluparri03 <gianlucaparri03@gmail.com>
2021-02-24 16:32:44 +0100

    First commit

---
+++ application/routes.py
@@ -0,0 +1,11 @@
+from flask import render_template
+from . import app
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
---
+++ run.py
@@ -0,0 +1,4 @@
+from application import app
+
+
+app.run(host='0.0.0.0', port=8888, debug=True)
