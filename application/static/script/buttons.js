75300cd8ca9b7599460ffa3eed5c7da920859f66
gianluparri03 <gianlucaparri03@gmail.com>
2021-05-08 12:08:32 +0200

    Added moveToTop and moveToBottom

--- application/static/script/buttons.js
+++ application/static/script/buttons.js
@@ -30,6 +30,20 @@ function moveUp(button) {
     }
 }
 
+function moveToTop(button) {
+    let element = button.parentNode.parentNode;
+    let list = element.parentNode;
+    let prev = element.previousElementSibling;
+
+    while (prev) {
+        list.insertBefore(element, prev);
+        prev.children[1].value++;
+        prev = element.previousElementSibling;
+    }
+
+    element.children[1].value = 1;
+}
+
 function moveDown(button) {
     let element = button.parentNode.parentNode;
     let list = element.parentNode;
@@ -40,6 +54,20 @@ function moveDown(button) {
         element.children[1].value++;
         next.children[1].value--;
     }
+}
+
+function moveToBottom(button) {
+    let element = button.parentNode.parentNode;
+    let list = element.parentNode;
+    let next = element.nextElementSibling;
+
+    while (next) {
+        list.insertBefore(next, element);
+        next.children[1].value--;
+        next = element.nextElementSibling;
+    }
+
+    element.children[1].value = list.childElementCount;
 }
 
 function confirmDeletion() {
